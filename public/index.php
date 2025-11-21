ffc23c694901ea440cfbceabed5555475a8b84f8
LooMac <louismcphee286@gmail.com>
2025-11-21 21:47:50 +0000

    Remove old HTML and CSS files; add new structured files for the music reviewing platform

---
+++ public/index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Revu ~ The Music Reviewing Hub</title>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link rel="stylesheet" href="/public/css/index_style.css">
+        <link rel="icon" type="image/png" href="assets/images/favicon.png">
+    </head>
+    <?php
+        include 'header.php';
+    ?>
+    <body>
+    </body>
+</html>
