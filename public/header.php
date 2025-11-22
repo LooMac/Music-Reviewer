5e8de4162aec25a1107f8de2a3240de26392b320
LooMac <louismcphee286@gmail.com>
2025-11-22 00:11:58 +0000

    Refactor header structure and update styles for improved layout and navigation

--- public/header.php
+++ public/header.php
@@ -1,12 +1,13 @@
-<header class="main-header">
-    <!-- This is the main title/nav element, present on all pages -->
-    <h1><a href="index.html">Revu</a></h1>
-    <h3>Your go-to platform for music reviews from <b>real</b> music listeners</h3>
-    <!-- This is the main horizontal navigation menu-->
+<header>
+    <div class="main-header">
+        <h1><a href="index.php">Revu</a></h1>
+        <h3>Your go-to platform for music reviews from <b>real</b> music fans</h3>
+    </div>
+    <hr>
     <nav class="main-nav">
         <ul>
-            <li><a href="index.html"><h2>Home</h2></a></li>
-            <li><a href="reviews.html"><h2>Reviews</h2></a></li>
+            <li><a href="browse.php"><h3>Browse</h3></a></li>
+            <li><a href="forum.php"><h3>Forum</h3></a></li>
         </ul>
     </nav>
 </header>
